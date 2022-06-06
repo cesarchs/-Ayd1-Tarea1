579e6125eb7c3f5a19b6e99aed47568b8f896029
cesarchs <cesarchamalesican@gmail.com>
2022-06-06 11:30:07 -0600

    se agrego funcionalidad de resta a la calculadora

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/',(req, res) => {
+    res.send('calculadora con node');
+});
+
+app.post('/api/suma',(req,res) => {
+    num1 = req.body.num1
+    num2 = req.body.num2
+   res.send ((parseInt(num1) + parseInt(num2)).toString())
+});
+
+app.post('/api/resta',(req,res) => {
+    num1 = req.body.num1
+    num2 = req.body.num2
+res.send ((parseInt(num1) - parseInt(num2)).toString())
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log('escuchando en el puerto 3000'))
